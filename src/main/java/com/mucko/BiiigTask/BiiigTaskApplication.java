37dbea95bf8319e32d5dc83a550b40088d550551
Filip <ext-filip.mucko@here.com>
2022-12-06 18:18:05 +0100

    Setting up a project

---
+++ src/main/java/com/mucko/BiiigTask/BiiigTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mucko.BiiigTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiiigTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiiigTaskApplication.class, args);
+	}
+
+}
